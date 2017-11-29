4df87fa15ef16786765e027ce51999502324d164
Ammybinks <nye@blythe.plus.com>
2017-11-29 00:59:19 +0000

    Initial commit

---
+++ src/ShowTime.java
@@ -0,0 +1,13 @@
+import java.util.Calendar;
+
+public class ShowTime {
+
+	public static void main(String[] args) {
+		while(true)
+		{
+		System.out.println(Calendar.getInstance().getTime());
+		}
+
+	}
+
+}
